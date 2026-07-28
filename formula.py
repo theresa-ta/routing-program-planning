305080013b15bcedae65049c0c17f2bfaf77834a
theresa <thereesaa_ta@yahoo.com>
2026-07-28 14:16:58 -0500

    while loop to calulcate time r/t distance

--- formula.py
+++ formula.py
@@ -1,4 +1,12 @@
-distance = int(input())
+truck_speed = 16 #created a while loop to help get distance and time for each stop
 
-truck_speed = 16
-travel_time = (distance / truck_speed) * 60 
+while True:
+    distance = float(input())
+
+    if distance == -1:
+        break
+    
+    travel_time = (distance / truck_speed) * 60
+    minutes = int(travel_time)
+    seconds = round((travel_time - minutes) * 60)
+    print(f"{minutes} minutes, {seconds} seconds.") 
