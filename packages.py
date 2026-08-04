91fa2cb831b5b53bdbc3ef30c1ea6a9a609df592
theresa <thereesaa_ta@yahoo.com>
2026-08-03 20:38:47 -0500

    Added 5 packages and a new file

---
+++ packages.py
@@ -0,0 +1,55 @@
+class Package:
+    def __init__(self, package_id, address, city, zipcode, deadline):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.zipcode = zipcode
+        self.deadline = deadline
+
+package1 = Package(
+    1,
+    "195 W Oakland Ave",
+    "Salt Lake City",
+    "84115",
+    "1030"
+)
+
+package2 = Package(
+    2,
+    "2530 S 500 E",
+    "Salt Lake City",
+    "84106",
+    "EOD"
+)
+
+package3 = Package(
+    3,
+    "233 Canyon Rd",
+    "Salt Lake City",
+    "84103",
+    "EOD"
+)
+
+package4 = Package(
+    4,
+    "380 W 2880 S",
+    "Salt Lake City",
+    "84115",
+    "EOD"
+)
+
+package5 = Package(
+    5,
+    "410 S State St,
+    "Salt Lake City",
+    "84111",
+    "EOD"
+)
+
+package1 = Package(
+    1,
+    "195 W Oakland Ave",
+    "Salt Lake City",
+    "84115",
+    "1030"
+)
