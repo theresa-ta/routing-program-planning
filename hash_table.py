9209652c4056b90c232114fc7322043ff7f72dfc
theresa <thereesaa_ta@yahoo.com>
2026-08-06 22:16:55 -0500

    created a hashtable that created empty table, hashkey, and insert

---
+++ hash_table.py
@@ -0,0 +1,13 @@
+#no import needed even though im referencing package_id?
+
+class HashTable:
+    def __init__(self): #first we make a hash table. 40 rows filled with none so it can be inserted w packages
+        self.table = [None] * 40
+
+    def hashkey(self, key): #create key, hash table need unique ID to access/hash
+        return key - 1 #since list start at 0, key - 1 will place package # to index #. ex: package 1 = 1 - 1 = 0, package 1 is the first package so it should go to list[0].
+    
+    def insert(self, package): #create insert to be able to insert package into hash table self.table
+        bucket = self.hashkey(package.package_id)
+        self.table[bucket] = package
+
