e08279e5dd5da838d42a1c4b8947afd8700f1aaa
theresa <thereesaa_ta@yahoo.com>
2026-08-10 11:56:46 -0500

    created a terminal intutiive interface

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
+#Student ID: 011512781
+
+#import files 
 from packages import packages
 from hash_table import HashTable
 
-hash_table = HashTable() #class HashTable just creates a blueprint. in order to use, build the object. variable hash_table builds object HashTable() from class HashTable
+#class HashTable just creates a blueprint. in order to use, build the object. variable hash_table builds object HashTable() from class HashTable
+hash_table = HashTable() 
 
-for package in packages: #iterates through the list packages in packages.py
+#iterates through the list packages in packages.py to insert each object/package into the hash table using ID. 
+for package in packages: 
     hash_table.insert(package)
 
 #print(hash_table.table[1].package_id) --> test to see if it worked. 
 
-"""
-Develop a look-up function that takes the package ID as input and returns each of the following corresponding data components:
-
-•   delivery address
-
-•   delivery deadline
-
-•   delivery city
-
-•   delivery zip code
-
-•   package weight
-
-•   delivery status (i.e., delayed, at the hub, en route, or delivered), including the delivery time
-so am i creating a search here?
-"""
-
-#package_test = hash_table.lookup(1)
+#package_test = hash_table.lookup(1) --> test. it worked
 #print(package_test.package_id)
 #print(package_test.status)
+
+#terminal intuitive interface -> delivery status, delivery time, any package time, total mileage
+print("WGUPUS Terminal Intuitive Interface\n1. Look up package\n2. View total mileage\n3. Exit")
