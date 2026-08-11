91822c31581fd222bcc09b8b6628ca62f2f1fe24
theresa <thereesaa_ta@yahoo.com>
2026-08-10 20:44:43 -0500

    added truck route

--- main.py
+++ main.py
@@ -18,12 +18,14 @@ for package in packages:
 #print(package_test.package_id)
 #print(package_test.status)
 
-group1 = [packages[12], packages[13], packages[14], packages[15], packages[18], packages[19], packages[20], packages[33], packages[38]] #9, has to be on the same truck/delievered together
+hub = "hub" #where western resides. base point
+
+group1 = [packages[12], packages[13], packages[14], packages[15], packages[18], packages[19], packages[33], packages[38]] #8, has to be on the same truck/delievered together
 group2 = [packages[0], packages[3], packages[6], packages[7], packages[28], packages[29], packages[30], packages[36], packages[39]] #9 time constraint
 group3 = [packages[5], packages[24], packages[25], packages[27], packages[31]] #5 delayed arrival 0905 delievered by 1030
 group4 = [packages[2], packages[4], packages[17], packages[35], packages[37]] #5, must be on truck 2
 group5 = [packages[10], packages[11], packages[16], packages[21], packages[22], packages[23], packages[26], packages[34]] #8 truck1b 
-group6 = [packages[1], packages[32], packages[8], packages[9]] #4 rest of truck 2
+group6 = [packages[1], packages[32], packages[8], packages[9], packages[20]] #5 rest of truck 2
 
 
 truck1 = Truck(
@@ -43,6 +45,47 @@ truck2.packages_truck.extend(group4)
 
 #for package in truck1.packages_truck: checking to see if it worked
     #print(package.package_id)
+
+#list containing truck route from, to. hub -> 14, mileage. 
+truck1_route = [
+    (14, 1.9),
+    ([15, 16, 34], 2.0),
+    (22, 4.7),
+    (24, 3.1),
+    (19, 2.5),
+    (20, 0.5),
+    (17, 2.4),
+    ([27, 35], 4.6),
+    ([13, 39], 1.6),
+    (12, 7.2),
+    (23, 7.5),
+    (11, 0.4),
+    (hub, 6.4)
+    ] #16
+truck2_route = [
+    ([40, 4], 3.6),
+    (1, 1.1),
+    ([30, 8], 4.5),
+    ([37, 38, 5], 1.0),
+    ([29, 7], 4.3),
+    (hub, 3.8)
+    ]
+
+#remove truck2 packages from list here to do truck2b route
+truck2b_route = [
+    ([25, 26], 2.4),
+    (6, 7.2),
+    ([31, 32], 1.5),
+    (36, 3.4),
+    (18, 4.0), 
+    (21, 5.3),
+    (28, 1.2),
+    ([2, 33], 1.1),
+    (3, 4.8),
+    (9, 0.6),
+    (10, 2.8),
+    (hub, 5.0)
+    ]
 
 
 #terminal intuitive interface -> delivery status, delivery time, any package time, total mileage
